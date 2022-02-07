b81d5ba10d3e50cc201227bfeaac351bc07d39d0
Sakib-Dev1 <dev.secondary1.acc@gmail.com>
2022-02-07 23:04:37 +0600

    Bismillah

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,21 @@
+//Selecting things
+//Navigation
 const menu = document.querySelector(".menu");
 const btnHamburger = document.querySelector(".hamburger");
 const btnClose = document.querySelector("#btn-close");
+//Cart
+const cartBtn = document.querySelector(".cart");
+const mainCart = document.querySelector(".main-cart");
+//Counter
+const minusBtn = document.querySelector("#btn-minus");
+const plusBtn = document.querySelector("#btn-plus");
+const productCounter = document.querySelector(".counter");
+// Gallery
+const manImg = document.querySelector(".product-img");
+const thumbSingleImg = document.querySelector(".image");
+
+// Initial count
+let counterValue = 1;
 
 // Show Menu
 btnHamburger.addEventListener("click", () => {
@@ -13,8 +28,24 @@ btnClose.addEventListener("click", () => {
 });
 
 // Show and Hide Cart
-const cartBtn = document.querySelector(".cart");
-const mainCart = document.querySelector(".main-cart");
+
 cartBtn.addEventListener("click", () => {
   mainCart.classList.toggle("hidden");
 });
+
+// Plus Count
+plusBtn.addEventListener("click", () => {
+  console.log("click");
+  setProductCounter(1);
+});
+// Minus Count
+minusBtn.addEventListener("click", () => {
+  setProductCounter(-1);
+});
+
+const setProductCounter = (value) => {
+  if (counterValue + value > 0) {
+    counterValue += value;
+    productCounter.innerHTML = counterValue;
+  }
+};
